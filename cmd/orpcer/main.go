adaae23965edc3430b65f4e4e59d484d24da3bb8
Dennis Kern <dennis.kern@axelspringer.com>
2020-08-20 08:36:09 +0200

    remove infinite loop, and remove print config

--- cmd/orpcer/main.go
+++ cmd/orpcer/main.go
@@ -1,42 +1,35 @@
 package main
 
 import (
-	"time"
-
 	"orpcer/config"
 	dynamodb "orpcer/dynamodb"
 	"orpcer/orphan"
+	"os"
 
 	"orpcer/log"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
 	cfg := config.Get()
-	spew.Dump(cfg)
 
 	logger := log.SetupLogger(cfg.LogLevel, cfg.LogFormat)
 
 	repo := dynamodb.NewRepo(cfg)
 	client := orphan.NewClient(repo, cfg)
 
-	for {
-		certs, err := client.GetOrphanCerts()
-		if err != nil {
-			logger.Error(err)
+	certs, err := client.GetOrphanCerts()
+	if err != nil {
+		logger.Error(err)
+		os.Exit(1)
+	}
 
+	if len(certs) > 0 {
+		logger.Infof("Found cert(s) which older than %d days", cfg.MaxAge)
+		for _, cert := range certs {
+			tmpl := "-> domain: %s, age: %d, created at: %s"
+			logger.Infof(tmpl, cert.Domain, cert.Age, cert.CreatedAt)
 		}
-
-		if len(certs) > 0 {
-			logger.Infof("Found cert(s) which older than %d days", cfg.MaxAge)
-			for _, cert := range certs {
-				tmpl := "-> domain: %s, age: %d, created at: %s"
-				logger.Infof(tmpl, cert.Domain, cert.Age, cert.CreatedAt)
-			}
-		} else {
-			logger.Infof("no-orphan-certs-found")
-		}
-		time.Sleep(time.Minute * 60)
+	} else {
+		logger.Infof("no-orphan-certs-found")
 	}
 }
