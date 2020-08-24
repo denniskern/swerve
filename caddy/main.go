433be34fb5d4087d1ebfc877f2f2cff11e7e42b4
Dennis Kern <dennis.kern@axelspringer.com>
2020-08-24 14:44:41 +0200

    caddy run 443, single instanz and digital15.swervetest.de

---
+++ caddy/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/caddyserver/certmagic"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	cache := certmagic.NewCache(certmagic.CacheOptions{
+		GetConfigForCert: func(cert certmagic.Certificate) (*certmagic.Config, error) {
+			// do whatever you need to do to get the right
+			// configuration for this certificate; keep in
+			// mind that this config value is used as a
+			// template, and will be completed with any
+			// defaults that are set in the Default config
+			return &certmagic.Config{
+				// ...
+			}, nil
+		},
+	})
+
+	magic := certmagic.New(cache, certmagic.Config{})
+
+	myACME := certmagic.NewACMEManager(magic, certmagic.ACMEManager{
+		CA:                   certmagic.LetsEncryptStagingCA,
+		Email:                "you@yours.com",
+		Agreed:               true,
+		DisableHTTPChallenge: true,
+	})
+
+	magic.Issuer = myACME
+
+	err := magic.ManageSync([]string{"digital15.swervetest.de"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tlsConfig := magic.TLSConfig()
+
+	s := &http.Server{
+		Addr:      ":443",
+		TLSConfig: tlsConfig,
+		Handler:   mux,
+	}
+
+	log.Fatal(s.ListenAndServeTLS("", ""))
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/aws/aws-sdk-go v1.33.4
+	github.com/caddyserver/certmagic v0.11.2
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/mock v1.4.3
 	github.com/gorilla/mux v1.7.4
